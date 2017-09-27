06264c9ee456ce12cf4787ceaedc0c3228e16cd9
Brian Lau <lauwangtatbrian@gmail.com>
2017-09-26 17:15:20 -0700

    set lat and long to vancouver

--- src/container/Map/Map.js
+++ src/container/Map/Map.js
@@ -1,24 +1,43 @@
 import React, { Component } from 'react';
-import MapGL from 'react-map-gl';
+import MapGL, { Popup, Marker } from 'react-map-gl';
 
 class Map extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            viewport: {
+                latitude: 49.248218,
+                longitude: -123.132136,
+                zoom: 11,
+                width: 800,
+                height: 400,
+            },
+        };
+    }
+    updateViewport = (viewport) => {
+        this.setState({ viewport });
+    }
     render() {
-        const map = (
-            <div>
-                <MapGL
-                    width={700}
-                    height={450}
-                    latitude={37.768}
-                    longitude={-122.331}
-                    zoom={9.017}
-                    mapboxApiAccessToken={"pk.eyJ1IjoiYnJpYW5sYXUiLCJhIjoiY2o4MjB5YWJnNnlzODMzdDYwd2xnbTZ5NyJ9.wqbtVC_Uv45yu9k76Vu_qg"}
+        const { viewport } = this.state;
+        const TOKEN = "pk.eyJ1IjoiYnJpYW5sYXUiLCJhIjoiY2o4MjB5YWJnNnlzODMzdDYwd2xnbTZ5NyJ9.wqbtVC_Uv45yu9k76Vu_qg"
 
-                />
-            </div>
-        )
         return (
             <div>
-                {map}
+                <MapGL
+                    {...viewport}
+                    mapStyle="mapbox://styles/mapbox/streets-v9"
+                    onViewportChange={this.updateViewport}
+                    mapboxApiAccessToken={TOKEN}
+                >
+                    <Marker
+                        latitude={37.768}
+                        longitude={-122.331}
+                        closeOnClick={true}
+                        dynamicPosition={false}
+                        tipSize={55}>
+                        <h1>.</h1>
+                    </Marker>
+                </MapGL>
             </div>
 
         )
